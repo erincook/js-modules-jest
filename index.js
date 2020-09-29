c2413dbd5fd61734360344dae0f83f32ce1f3f8b
Zelalem <zelalemtenaw26@gmail.com>
2020-09-29 12:17:10 -0400

    Edited index.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-console.log("from index.js"); 
+console.log("from index.js");
 
-import add from './modules/calculator.js'; 
+import add from "./modules/calculator";
+import subtract from "./modules/calculator";
 
-console.log(add(3, 4)); 
+console.log(add(3, 4));
+console.log(subtract(4, 3));
